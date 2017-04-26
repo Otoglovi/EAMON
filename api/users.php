774555ef15eaa1dd7cacdeb93543516ce2ec4446
Otoglovi <darlington.2004@gmail.com>
2017-04-26 16:15:07 +0100

    api commit for update and delete

--- api/users.php
+++ api/users.php
@@ -85,3 +85,58 @@ function insert_user()
     header('Content-Type: application/json');
     echo json_encode($response);
 }
+
+function update_user($user_id)
+{
+    global $connection;
+    parse_str(file_get_contents("php://input"), $post_vars);
+    $user_name = $post_vars["username"];
+    $password = $post_vars["password"];
+    $email = $post_vars["email"];
+    $phone_number = $post_vars["phone"];
+    $user_type = $post_vars["type"];
+    $full_name = $post_vars["fullName"];
+
+    $query = "UPDATE users SET username='{$user_name}', password={$password},
+    email={$email}, phone= {$phone_number}, type={$user_type}, fullName='{$full_name}' WHERE id =".$user_id;
+
+    if (mysqli_query($connection, $query))
+    {
+        $response=array(
+            'status' => 1,
+            'status_message' =>'User Updated Successfully.'
+        );
+    }
+    else
+    {
+        $response=array(
+            'status' => 0,
+            'status_message' =>'User Updation Failed.'
+        );
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+function delete_user($user_id)
+{
+    global $connection;
+    $query = "DELETE FROM users WHERE id =".$user_id;
+    if (mysqli_query($connection, $query))
+    {
+        $response=array(
+            'status' => 1,
+            'status_message' =>'User Deleted Successfully.'
+        );
+    }
+    else
+    {
+        $response=array(
+            'status' => 0,
+            'status_message' =>'User Deletion Failed.'
+        );
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
