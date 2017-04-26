6f8c6040b465a3d2311aa25f42b7c881b27b03da
Otoglovi <darlington.2004@gmail.com>
2017-04-26 15:41:08 +0100

    api commit

---
+++ api/users.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Code below was developed with the help of :
+ * https://www.apptha.com/blog/how-to-build-a-rest-api-using-php/
+ *  By: Mohammed Bilal Shareef
+ */
+
+//Database connection
+$connection = mysqli_connect('', '', '', 'rest_api');
+
+$request_method = $_SERVER["REQUEST_METHOD"];
+switch ($request_method) {
+    case 'GET':
+        //Retrieve Users
+        if (!empty($_GET["user_id"])) {
+            $user_id = intval($_GET["user_id"]);
+            get_users($user_id);
+        } else {
+            get_users();
+        }
+        break;
+    case 'POST':
+        //Insert User
+        insert_user();
+        break;
+    case 'PUT':
+        //Update User
+        $user_id = intval($_GET["user_id"]);
+        update_user($user_id);
+        break;
+    case 'DELETE':
+        //Delete User
+        $user_id = intval($_GET["user_id"]);
+        delete_user($user_id);
+        break;
+    default:
+        //Invalid Request Method
+        header("HTTP/1.0 405 Method Not Allowed");
+        break;
+}
+
+function get_users($user_id = 0)
+{
+    global $connection;
+    $query = "SELECT * FROM users";
+    if ($user_id != 0) {
+        $query .= " WHERE id=" . $user_id . " LIMIT 1";
+    }
+    $response = array();
+    $result = mysqli_query($connection, $query);
+    while ($row = mysqli_fetch_array($result)) {
+        $response[] = $row;
+    }
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
+
+function insert_user()
+{
+    global $connection;
+    $user_name = $_POST["username"];
+    $password = $_POST["password"];
+    $email = $_POST["email"];
+    $phone_number = $_POST["phone"];
+    $user_type = $_POST["type"];
+    $full_name = $_POST["fullName"];
+    $query = "INSERT INTO users SET username='{$user_name}', password='{$password}',
+    email='{$email}', phone= '{$phone_number}', type='{$user_type}', fullName='{$full_name}'";
+
+    if (mysqli_query($connection, $query))
+    {
+        $response = array(
+            'status' => 1,
+            'status_message' => 'User Added Successfully.'
+        );
+    }
+    else
+    {
+        $response = array(
+            'status' => 0,
+            'status_message' => 'User Addition Failed.'
+        );
+    }
+
+    header('Content-Type: application/json');
+    echo json_encode($response);
+}
