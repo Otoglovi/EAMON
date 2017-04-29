ba8bc8a9e7e9f16d3df0fb70c4a9abdf4ae39d83
Otoglovi <darlington.2004@gmail.com>
2017-04-29 23:58:54 +0100

    select query in users.php

--- api/users.php
+++ api/users.php
@@ -45,27 +45,11 @@ switch ($request_method) {
 
 function get_users($users)
 {
-    //echo $users."WE ARE   INSIDE ";
     global $link;
-    $query = "SELECT * FROM users";
+    $query = "SELECT `id`, `username`,`email`, `phone`, `type`, `fullName` FROM users";
     if (strlen($users) > 0) {
-        /** @var TYPE_NAME $users */
         $query .= " WHERE id=" . $users . " LIMIT 1";
     }
-    /*   $response = array();
-
-       $result = mysqli_query($link, $query);
-       while ($row = mysqli_fetch_array($result)) {
-           $response[] = $row;
-          // echo var_dump($response[])." I N  SQL";
-       }
-       echo var_dump($response[])." after SQL";
-   //    header('Content-Type: application/json');
-   //    echo json_encode($response);
-   //    echo $users;
-   }
-
-   */
     $response = array();
     $result = mysqli_query($link, $query);
     $row_cnt = $result->num_rows;
