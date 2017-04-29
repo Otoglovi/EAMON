381f86d2cfe4ba7c2fc73a7f668668e1fb69e361
Otoglovi <darlington.2004@gmail.com>
2017-04-29 13:32:24 +0100

    update web.config  tty

--- api/users.php
+++ api/users.php
@@ -52,94 +52,13 @@ function get_users($users = 0)
     while ($row = mysqli_fetch_array($result)) {
         $response[] = $row;
     }
-    header('Content-Type: application/json');
-    echo json_encode($response);
+//    header('Content-Type: application/json');
+//    echo json_encode($response);
+    echo $users;
 }
 
-function insert_user()
-{
-    global $connection;
-    $user_name = $_POST["username"];
-    $password = $_POST["password"];
-    $email = $_POST["email"];
-    $phone_number = $_POST["phone"];
-    $user_type = $_POST["type"];
-    $full_name = $_POST["fullName"];
-    $query = "INSERT INTO users SET username='{$user_name}', password='{$password}',
-    email='{$email}', phone= '{$phone_number}', type='{$user_type}', fullName='{$full_name}'";
 
-    if (mysqli_query($connection, $query))
-    {
-        $response = array(
-            'status' => 1,
-            'status_message' => 'User Added Successfully.'
-        );
-    }
-    else
-    {
-        $response = array(
-            'status' => 0,
-            'status_message' => 'User Addition Failed.'
-        );
-    }
 
-    header('Content-Type: application/json');
-    echo json_encode($response);
-}
 
-function update_user($user_id)
-{
-    global $connection;
-    parse_str(file_get_contents("php://input"), $post_vars);
-    $user_name = $post_vars["username"];
-    $password = $post_vars["password"];
-    $email = $post_vars["email"];
-    $phone_number = $post_vars["phone"];
-    $user_type = $post_vars["type"];
-    $full_name = $post_vars["fullName"];
-
-    $query = "UPDATE users SET username='{$user_name}', password={$password},
-    email={$email}, phone= {$phone_number}, type={$user_type}, fullName='{$full_name}' WHERE id =".$user_id;
-
-    if (mysqli_query($connection, $query))
-    {
-        $response=array(
-            'status' => 1,
-            'status_message' =>'User Updated Successfully.'
-        );
-    }
-    else
-    {
-        $response=array(
-            'status' => 0,
-            'status_message' =>'User Updation Failed.'
-        );
-    }
-    header('Content-Type: application/json');
-    echo json_encode($response);
-}
-
-function delete_user($user_id)
-{
-    global $connection;
-    $query = "DELETE FROM users WHERE id =".$user_id;
-    if (mysqli_query($connection, $query))
-    {
-        $response=array(
-            'status' => 1,
-            'status_message' =>'User Deleted Successfully.'
-        );
-    }
-    else
-    {
-        $response=array(
-            'status' => 0,
-            'status_message' =>'User Deletion Failed.'
-        );
-    }
-
-    header('Content-Type: application/json');
-    echo json_encode($response);
-}
 //Close database connection
 mysqli_close($connection);
