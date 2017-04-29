3c70caed21f668a50ea81958a6383c1120e22e4f
Otoglovi <darlington.2004@gmail.com>
2017-04-29 15:05:19 +0100

    update web.config  tty

--- api/users.php
+++ api/users.php
@@ -70,7 +70,7 @@ $response = array();
 $result = mysqli_query($link, $query);
 $row_cnt = $result->num_rows;
 if ($row_cnt >0) {
-    while ($row = mysqli_fetch_assoc($result))
+    while ($row = mysqli_fetch_array($result))
     {
         $response[] = $row;
     }
