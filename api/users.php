a495c2d91dd6aaaa432a6323e1dc468f032fe83a
Otoglovi <darlington.2004@gmail.com>
2017-04-29 14:36:57 +0100

    update web.config  tty

--- api/users.php
+++ api/users.php
@@ -54,6 +54,7 @@ function get_users($users)
     }
     $response = array();
 
+  //  $query = mysqli_query($link, "SELECT * FROM users")
     $result = mysqli_query($link, $query);
     while ($row = mysqli_fetch_array($result)) {
         $response[] = $row;
