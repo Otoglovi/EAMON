ec06e726b2eb37af46ff7ddf0cd79e6caf4a353d
Otoglovi <darlington.2004@gmail.com>
2017-05-05 01:23:11 +0100

    update post web config rules

--- api/users.php
+++ api/users.php
@@ -88,7 +88,9 @@ function insert_users($user)
     $phone=$user[3];
     $type=$user[4];
     $fullName=$user[5];
-    $query="INSERT INTO 'users'(username, password, email, phone, type, fullName) values ('$username','$password','$email','$phone','$type','$fullName')";
+    $query="INSERT INTO `users` (username`, `password`, `email`, `phone`, `type`, `fullName`)
+            VALUES
+	       ('$username','$password','$email','$phone','$type','$fullName');";
         echo $query;
 
     mysqli_query($link, $query) or die(mysqli_error($link));
